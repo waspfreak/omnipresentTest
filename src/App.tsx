3ccce20900071394a667c6d50f30c3450160dfcf
juliana <juliana.leon@tigerspike.com>
2021-11-04 19:09:37 +0000

    small fix imaginary logo

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<div data-testid="app-id">
 			<AppStyle>
-				<Header label='Omnipresent Form' />
+				<Header label='Omnipresent' />
 				<Form />
 			</AppStyle>
 		</div>
